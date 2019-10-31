6c3690fdee4f14dbb1813552e59d300375f91835
黄丙辉 <1437275916@qq.com>
2019-10-31 17:17:07 +0800

    Update romenToInt.java

--- src/practise/romenToInt.java
+++ src/practise/romenToInt.java
@@ -7,3 +7,21 @@ package practise;
 *@2019-10-31
 */
 
+public class romenToInt{
+  public int romenToInt(String s){
+    Hashtable<String,Integer> romen = new Hashtable<String,Integer>();
+    romen.put("I",1);
+    romen.put("V",5);
+    romen.put("X",10);
+    romen.put("L",50);
+    romen.put("C",100);
+    romen.put("D",500);
+    romen.put("M",1000);
+    
+    
+    int sum = 0;
+    for(int i = 1;i<s.length();){
+      if ()
+    }
+  }
+}
